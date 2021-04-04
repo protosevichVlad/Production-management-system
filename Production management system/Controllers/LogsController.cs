078ec8dbc65e973926e74a688a20aeb921ef2563
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-04-04 22:46:47 +0300

    feat: add logs view for all entities

--- Production management system/Controllers/LogsController.cs
+++ Production management system/Controllers/LogsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -27,9 +28,40 @@ namespace ProductionManagementSystem.Controllers
             }).CreateMapper();
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string userName, int? deviceId, int? componentId, int? designId, int? taskId, int? orderId)
         {
             var logs = _mapper.Map<IEnumerable<LogDTO>, IEnumerable<LogViewModel>>(_logService.GetLogs());
+
+            if (userName != null)
+            {
+                logs = logs.Where(l => l.UserLogin == userName);
+            }
+            
+            if (deviceId != null)
+            {
+                logs = logs.Where(l => l.DesignId == deviceId);
+            }
+            
+            if (componentId != null)
+            {
+                logs = logs.Where(l => l.ComponentId == componentId);
+            }
+            
+            if (designId != null)
+            {
+                logs = logs.Where(l => l.DesignId == designId);
+            }
+            
+            if (taskId != null)
+            {
+                logs = logs.Where(l => l.TaskId == taskId);
+            }
+            
+            if (orderId != null)
+            {
+                logs = logs.Where(l => l.OrderId == orderId);
+            }
+            
             return View(logs);
         }
         
