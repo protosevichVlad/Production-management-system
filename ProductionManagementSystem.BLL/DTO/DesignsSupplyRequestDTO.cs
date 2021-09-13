3f8b5bba3dd07f0f08412e29f05f8210521f6436
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-09-13 18:07:41 +0300

    feat: create DTO models and interfaces for supplyRequests

---
+++ ProductionManagementSystem.BLL/DTO/DesignsSupplyRequestDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ProductionManagementSystem.DAL.Entities;
+using ProductionManagementSystem.DAL.Enums;
+
+namespace ProductionManagementSystem.BLL.DTO
+{
+    public class DesignsSupplyRequestDTO
+    {
+        public int Id { get; set; }
+        public int? OrderId { get; set; }
+        public OrderDTO Order { get; set; }
+        public int DesignId { get; set; }
+        public DesignDTO Design { get; set; }
+        public UserDTO User { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DesiredDate { get; set; }
+        public int Quantity { get; set; }
+        public string Comment { get; set; }
+        public StatusSupplyEnumDTO StatusSupply { get; set; }
+    }
+}
---
+++ ProductionManagementSystem.BLL/Interfaces/IDesignsSupplyRequestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProductionManagementSystem.BLL.DTO;
+
+namespace ProductionManagementSystem.BLL.Interfaces
+{
+    public interface IDesignsSupplyRequestService : IDisposable
+    {
+        Task CreateDesignSupplyRequestAsync(DesignsSupplyRequestDTO componentsSupplyRequest);
+        Task UpdateDesignSupplyRequestAsync(DesignsSupplyRequestDTO componentsSupplyRequest);
+        Task<IEnumerable<DesignsSupplyRequestDTO>> GetDesignSupplyRequestsAsync();
+        Task<DesignsSupplyRequestDTO> GetDesignSupplyRequestAsync(int? id);
+        Task DeleteDesignSupplyRequestAsync(int? id);
+    }
+}
