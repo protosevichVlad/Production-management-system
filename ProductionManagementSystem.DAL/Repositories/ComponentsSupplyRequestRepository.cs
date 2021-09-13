558895efe1f1a086ab99976c3dc40c9e08d23b63
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-09-13 11:03:38 +0300

    feat: create ComponentsSupplyRequestRepository.cs

---
+++ ProductionManagementSystem.DAL/Repositories/ComponentsSupplyRequestRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.DAL.Entities;
+using ProductionManagementSystem.DAL.Interfaces;
+using Task = System.Threading.Tasks.Task;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public class ComponentsSupplyRequestRepository : IRepository<ComponentsSupplyRequest>
+    {
+        private ApplicationContext _db;
+
+        public ComponentsSupplyRequestRepository(ApplicationContext context)
+        {
+            _db = context;
+        }
+        
+        public async Task<IEnumerable<ComponentsSupplyRequest>> GetAllAsync()
+        {
+            return await _db.ComponentsSupplyRequests.ToListAsync();
+        }
+
+        public async Task<ComponentsSupplyRequest> GetAsync(int id)
+        {
+            return await _db.ComponentsSupplyRequests.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public IEnumerable<ComponentsSupplyRequest> Find(Func<ComponentsSupplyRequest, bool> predicate)
+        {
+            return _db.ComponentsSupplyRequests.Where(predicate).ToList();
+        }
+
+        public async Task CreateAsync(ComponentsSupplyRequest item)
+        {
+            await _db.ComponentsSupplyRequests.AddAsync(item);
+        }
+
+        public void Update(ComponentsSupplyRequest item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            ComponentsSupplyRequest componentsSupplyRequest = await GetAsync(id);
+            if (componentsSupplyRequest != null)
+                _db.ComponentsSupplyRequests.Remove(componentsSupplyRequest);
+        }
+    }
+}
