6dd77e341b014fae28c99b6969576cc2ded00aed
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-09-13 10:53:08 +0300

    feat: add Entities of SupplyRequest

---
+++ ProductionManagementSystem.DAL/Entities/DesignsSupplyRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ProductionManagementSystem.DAL.Enums;
+
+namespace ProductionManagementSystem.DAL.Entities
+{
+    public class DesignsSupplyRequest
+    {
+        public int Id { get; set; }
+        public int? OrderId { get; set; }
+        public Order Order { get; set; }
+        public int DesignId { get; set; }
+        public Design Design { get; set; }
+        public int ProductionManagementSystemUserId { get; set; }
+        public ProductionManagementSystemUser User { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DesiredDate { get; set; }
+        public int Quantity { get; set; }
+        public string Comment { get; set; }
+        public StatusSupplyEnum StatusSupply { get; set; }
+    }
+}
