62541b52425dad50d419afde2ae5e7f80f448d6b
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-09-21 13:19:48 +0300

    feat: create view models for two pages

---
+++ src/ProductionManagementSystem.WEB/ViewModels/ComponentsSupplyRequestViewModels/CreatePageComponentsSupplyRequestViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ProductionManagementSystem.DAL.Entities;
+
+namespace ProductionManagementSystem.WEB.ViewModels.ComponentsSupplyRequestViewModels
+{
+    public class CreatePageComponentsSupplyRequestViewModel
+    {
+        public ComponentsSupplyRequest ComponentsSupplyRequest { get; set; }
+    }
+}
---
+++ src/ProductionManagementSystem.WEB/ViewModels/ComponentsSupplyRequestViewModels/IndexPageComponentsSupplyRequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ProductionManagementSystem.WEB.Models;
+
+namespace ProductionManagementSystem.WEB.ViewModels.ComponentsSupplyRequestViewModels
+{
+    public class IndexPageComponentsSupplyRequestViewModel
+    {
+        public IEnumerable<ComponentsSupplyRequest> ComponentsSupplyRequests { get; set; }
+        public string OrderBy { get; set; }
+        public string CurrentFilter { get; set; }
+
+        public string Page { get; set; }
+        public string PageSize { get; set; }
+    }
+}
