a772e0bbef7fee1665c601238d05caa5ce9da5e7
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-09-21 13:20:18 +0300

    feat: add new models in web layer

---
+++ src/ProductionManagementSystem.WEB/Models/ComponentsSupplyRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProductionManagementSystem.WEB.Models
+{
+    public class ComponentsSupplyRequest
+    {
+        public int Id { get; set; }
+        public int? TaskId { get; set; }
+        public TaskViewModel Task { get; set; }
+        public int ComponentId { get; set; }
+        public ComponentViewModel Component { get; set; }
+        public UserViewModel User { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DesiredDate { get; set; }
+        public int Quantity { get; set; }
+        public string Comment { get; set; }
+        public StatusSupplyEnum StatusSupply { get; set; }
+    }
+}
---
+++ src/ProductionManagementSystem.WEB/Models/StatusSupplyEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductionManagementSystem.WEB.Models
+{
+    public enum StatusSupplyEnum
+    {
+        [Display(Name="Не принята")]
+        NotAccepted = 1,
+        [Display(Name="В работе")]
+        InWork = 2,
+        [Display(Name="Заказано")]
+        Ordered = 4,
+        [Display(Name="Готова")]
+        Ready = 8,
+    }
+}
