edfecdd132bfc3b98567a8f12b06c221948d8164
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-19 20:39:33 +0300

    refactor: applicationContext

--- src/ProductionManagementSystem.DAL/EF/ApplicationContext.cs
+++ src/ProductionManagementSystem.DAL/EF/ApplicationContext.cs
@@ -1,28 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using ProductionManagementSystem.DAL.Entities;
-using System;
+using ProductionManagementSystem.Models.Components;
+using ProductionManagementSystem.Models.Devices;
+using ProductionManagementSystem.Models.Logs;
+using ProductionManagementSystem.Models.Orders;
+using ProductionManagementSystem.Models.SupplyRequests;
+using ProductionManagementSystem.Models.Tasks;
+using ProductionManagementSystem.Models.Users;
 
 namespace ProductionManagementSystem.DAL.EF
 {
-    public class ApplicationContext : IdentityDbContext<ProductionManagementSystemUser>
+    public class ApplicationContext : IdentityDbContext<User>
     {
-        public DbSet<Device> Devices { get; set; }
-        public DbSet<Component> Components { get; set; }
-        public DbSet<DeviceComponentsTemplate> DeviceComponentsTemplates { get; set; }
-        public DbSet<DeviceDesignTemplate> DeviceDesignTemplates { get; set; }
-        
+        public DbSet<Montage> Montages { get; set; }
         public DbSet<Design> Designs { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<MontageInDevice> MontageInDevices { get; set; }
+        public DbSet<DesignInDevice> DesignInDevices { get; set; }
+        
         public DbSet<Task> Tasks { get; set; }
-        public DbSet<ObtainedComponent> ObtainedComponents { get; set; }
+        public DbSet<ObtainedMontage> ObtainedMontages { get; set; }
         public DbSet<ObtainedDesign> ObtainedDesigns { get; set; }
         
         public DbSet<Log> Logs { get; set; }
         public DbSet<Order> Orders { get; set; }
         
-        public DbSet<ComponentsSupplyRequest> ComponentsSupplyRequests { get; set; }
-        public DbSet<DesignsSupplyRequest> DesignsSupplyRequests { get; set; }
+        public DbSet<MontageSupplyRequest> ComponentSupplyRequests { get; set; }
+        public DbSet<DesignSupplyRequest> DesignSupplyRequests { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
