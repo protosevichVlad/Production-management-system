485e01467010524fd1cd0bdedc64acd711404691
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-20 22:32:09 +0300

    fix: class name

---
+++ src/ProductionManagementSystem.DAL/Repositories/DesignInDeviceRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Devices;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public interface IDesignInDeviceRepository : IRepository<DesignInDevice>
+    {
+        
+    }
+    public class DesignInDeviceRepository : Repository<DesignInDevice>, IDesignInDeviceRepository
+    {
+        public DesignInDeviceRepository(ApplicationContext db) : base(db)
+        {
+        }
+    }
+}
