7adc0bcc38b5f091fcc1c62435a167e5193cf08a
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-20 22:31:11 +0300

    refactor: obtained components

---
+++ src/ProductionManagementSystem.DAL/Repositories/ObtainedMontageRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Tasks;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public interface IObtainedMontageRepository : IRepository<ObtainedMontage>
+    {
+        
+    }
+
+    public class ObtainedMontageRepository : Repository<ObtainedMontage>, IObtainedMontageRepository
+    {
+        public ObtainedMontageRepository(ApplicationContext db) : base(db)
+        {
+        }
+    }
+}
