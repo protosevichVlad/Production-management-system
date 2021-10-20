f3affe7d32e2413c80306c19cb5a57d7870eb6fb
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-20 22:14:25 +0300

    refactor: supply requests repositories

--- src/ProductionManagementSystem.DAL/Repositories/DesignsSupplyRequestRepository.cs
+++ src/ProductionManagementSystem.DAL/Repositories/DesignsSupplyRequestRepository.cs
@@ -4,61 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ProductionManagementSystem.DAL.EF;
-using ProductionManagementSystem.DAL.Entities;
-using ProductionManagementSystem.DAL.Interfaces;
-using Task = System.Threading.Tasks.Task;
+using ProductionManagementSystem.Models.SupplyRequests;
 
 namespace ProductionManagementSystem.DAL.Repositories
 {
-    public class DesignsSupplyRequestRepository : IRepository<DesignsSupplyRequest>
+    public interface IDesignsSupplyRequestRepository : IRepository<DesignSupplyRequest>
     {
-        private ApplicationContext _db;
-
-        public DesignsSupplyRequestRepository(ApplicationContext context)
-        {
-            _db = context;
-        }
         
-        public async Task<IEnumerable<DesignsSupplyRequest>> GetAllAsync()
-        {
-            return await _db.DesignsSupplyRequests
-                .Include(d => d.Design)
-                .ToListAsync();
-        }
+    }
 
-        public async Task<DesignsSupplyRequest> GetAsync(int id)
+    public class DesignsSupplyRequestRepository : Repository<DesignSupplyRequest>, IDesignsSupplyRequestRepository
+    {
+        public DesignsSupplyRequestRepository(ApplicationContext db) : base(db)
         {
-            return await _db.DesignsSupplyRequests.FirstOrDefaultAsync(d => d.Id == id);
-        }
-
-        public IEnumerable<DesignsSupplyRequest> Find(Func<DesignsSupplyRequest, bool> predicate)
-        {
-            return _db.DesignsSupplyRequests.Where(predicate).ToList();
-        }
-
-        public async Task CreateAsync(DesignsSupplyRequest item)
-        {
-            await _db.DesignsSupplyRequests.AddAsync(item);
-        }
-
-        public void Update(DesignsSupplyRequest item)
-        {
-            var local = _db.Set<DesignsSupplyRequest>()
-                .Local
-                .FirstOrDefault(entry => entry.Id.Equals(item.Id));
-            if (local != null)
-            {
-                _db.Entry(local).State = EntityState.Detached;
-            }
-            
-            _db.Entry(item).State = EntityState.Modified;
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            DesignsSupplyRequest designsSupplyRequest = await GetAsync(id);
-            if (designsSupplyRequest != null)
-                _db.DesignsSupplyRequests.Remove(designsSupplyRequest);
         }
     }
 }
---
+++ src/ProductionManagementSystem.DAL/Repositories/MontageSupplyRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.SupplyRequests;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public interface IMontageSupplyRequestRepository : IRepository<MontageSupplyRequest>
+    {
+        
+    }
+    public class MontageSupplyRequestRepository : Repository<MontageSupplyRequest>, IMontageSupplyRequestRepository
+    {
+        public MontageSupplyRequestRepository(ApplicationContext db) : base(db)
+        {
+        }
+    }
+}
