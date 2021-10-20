c3eeaba3279cf37a1bb1849caa703f8fda4d6efa
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-20 22:21:11 +0300

    refactor: components in devices

---
+++ src/ProductionManagementSystem.DAL/Repositories/DesignIndDeviceRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Devices;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public interface IDesignIndDeviceRepository : IRepository<DesignInDevice>
+    {
+        
+    }
+    public class DesignIndDeviceRepository : Repository<DesignInDevice>, IDesignIndDeviceRepository
+    {
+        public DesignIndDeviceRepository(ApplicationContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ src/ProductionManagementSystem.DAL/Repositories/MontageInDeviceRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Devices;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public interface IMontageInDeviceRepository : IRepository<MontageInDevice>
+    {
+        
+    }
+
+    public class MontageInDeviceRepository : Repository<MontageInDevice>, IMontageInDeviceRepository
+    {
+        public MontageInDeviceRepository(ApplicationContext db) : base(db)
+        {
+        }
+    }
+}
