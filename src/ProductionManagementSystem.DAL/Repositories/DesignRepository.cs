5e991d8c2f0c1f70e7731e037b6bea653f85ffa5
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-20 13:13:23 +0300

    refactor: create montage and design repository

--- src/ProductionManagementSystem.DAL/Repositories/DesignRepository.cs
+++ src/ProductionManagementSystem.DAL/Repositories/DesignRepository.cs
@@ -1,54 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using ProductionManagementSystem.DAL.EF;
-using ProductionManagementSystem.DAL.Entities;
-using ProductionManagementSystem.DAL.Interfaces;
-using Task = System.Threading.Tasks.Task;
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Components;
 
 namespace ProductionManagementSystem.DAL.Repositories
 {
-    public class DesignRepository : IRepository<Design>
+    public interface IDesignRepository : IComponentBaseRepository<Design>
     {
-        private readonly ApplicationContext _db;
-
-        public DesignRepository(ApplicationContext context)
+        
+    }
+    public class DesignRepository : ComponentBaseRepository<Design>, IDesignRepository
+    {
+        public DesignRepository(ApplicationContext db) : base(db)
         {
-            _db = context;
-        }
-
-        public async Task<IEnumerable<Design>> GetAllAsync()
-        {
-            return await _db.Designs.ToListAsync();
-        }
-
-        public async Task<Design> GetAsync(int id)
-        {
-            return await _db.Designs.FindAsync(id);
-        }
-
-        public IEnumerable<Design> Find(Func<Design, bool> predicate)
-        {
-            return _db.Designs.Where(predicate).ToList();
-        }
-
-        public async Task CreateAsync(Design item)
-        {
-            await _db.Designs.AddAsync(item);
-        }
-
-        public void Update(Design item)
-        {
-            _db.Entry(item).State = EntityState.Modified;
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var item = await _db.Designs.FindAsync(id);
-            if (item != null)
-                _db.Designs.Remove(item);
         }
     }
 }
---
+++ src/ProductionManagementSystem.DAL/Repositories/MontageRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Components;
+
+namespace ProductionManagementSystem.DAL.Repositories
+{
+    public interface IMontageRepository : IComponentBaseRepository<Montage>
+    {
+        
+    }
+
+    public class MontageRepository : ComponentBaseRepository<Montage>, IMontageRepository
+    {
+        public MontageRepository(ApplicationContext db) : base(db)
+        {
+        }
+    }
+}
