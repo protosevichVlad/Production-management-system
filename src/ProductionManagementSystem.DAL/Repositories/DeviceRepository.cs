6c9e48212e403b8a7af2c9b9de0638d036988fcd
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-20 22:16:19 +0300

    refactor: DeviceRepository

--- src/ProductionManagementSystem.DAL/Repositories/DeviceRepository.cs
+++ src/ProductionManagementSystem.DAL/Repositories/DeviceRepository.cs
@@ -1,68 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using ProductionManagementSystem.DAL.EF;
-using ProductionManagementSystem.DAL.Entities;
-using ProductionManagementSystem.DAL.Interfaces;
-using Task = System.Threading.Tasks.Task;
+﻿using ProductionManagementSystem.DAL.EF;
+using ProductionManagementSystem.Models.Devices;
 
 namespace ProductionManagementSystem.DAL.Repositories
 {
-    public class DeviceRepository : IRepository<Device>
+    public interface IDeviceRepository : IRepository<Device>
     {
-        private readonly ApplicationContext _db;
-
-        public DeviceRepository(ApplicationContext context)
+        
+    }
+    public class DeviceRepository : Repository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(ApplicationContext db) : base(db)
         {
-            _db = context;
         }
-
-        public async Task<IEnumerable<Device>> GetAllAsync()
-        {
-            return await _db.Devices
-                .Include(d => d.DeviceComponentsTemplate)
-                    .ThenInclude(c => c.Component)
-                .Include(d => d.DeviceDesignTemplate)
-                    .ThenInclude(d => d.Design)
-                .ToListAsync();
-        }
-
-        public async Task<Device> GetAsync(int id)
-        {
-            return await _db.Devices
-                .Include(d => d.DeviceComponentsTemplate)
-                    .ThenInclude(c => c.Component)
-                .Include(d => d.DeviceDesignTemplate)
-                    .ThenInclude(d => d.Design).FirstOrDefaultAsync(d => d.Id == id);
-        }
-
-        public IEnumerable<Device> Find(Func<Device, bool> predicate)
-        {
-            return _db.Devices
-                .Include(d => d.DeviceComponentsTemplate)
-                .ThenInclude(c => c.Component)
-                .Include(d => d.DeviceDesignTemplate)
-                .ThenInclude(d => d.Design).Where(predicate);
-        }
-
-        public async Task CreateAsync(Device item)
-        {
-            await _db.Devices.AddAsync(item);
-        }
-
-        public void Update(Device item)
-        {
-            _db.Devices.Update(item);
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var item = await _db.Devices.FindAsync(id);
-            if (item != null)
-                _db.Devices.Remove(item);
-        }
-
     }
 }
