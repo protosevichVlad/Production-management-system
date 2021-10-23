205aa90d25b9d01682be0fdc6a8155f455835dce
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-23 13:40:45 +0300

    refactor: montage supply requests

---
+++ src/ProductionManagementSystem.BLL/Services/MontageSupplyRequestService.cs
@@ -0,0 +1,44 @@
+﻿using ProductionManagementSystem.BLL.DTO;
+using ProductionManagementSystem.BLL.Interfaces;
+using ProductionManagementSystem.DAL.Repositories;
+using ProductionManagementSystem.Models.SupplyRequests;
+using Task = System.Threading.Tasks.Task;
+
+namespace ProductionManagementSystem.BLL.Services
+{
+    public interface IMontageSupplyRequestService : IBaseService<MontageSupplyRequest>
+    {
+        Task ChangeStatusAsync(int id, int to, string message);
+    }
+
+    public class MontageSupplyRequestService : BaseService<MontageSupplyRequest>, IMontageSupplyRequestService
+    {
+        private readonly IMontageService _montageService;
+        private ILogService _log;
+        
+        public MontageSupplyRequestService(IUnitOfWork uow) : base(uow)
+        {
+            _montageService = new MontageService(uow);
+            _log = new LogService(uow);
+        }
+        
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public async Task ChangeStatusAsync(int id, int to, string message = "")
+        {
+            var montageSupplyRequest = await GetByIdAsync(id);
+            montageSupplyRequest.StatusSupply = (SupplyStatusEnum) to;
+            if ((SupplyStatusEnum) to == SupplyStatusEnum.Ready)
+            {
+                await _montageService.IncreaseQuantityOfMontageAsync(montageSupplyRequest.ComponentId,
+                    montageSupplyRequest.Quantity);
+            }
+            
+            Update(montageSupplyRequest);
+            await _log.CreateLogAsync(new LogDTO(message));
+        }
+    }
+}
