20077005efceed86085d3b1e8f17f60234222c8d
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-10-24 10:54:54 +0300

    feat: add method for finding components by device id

--- src/ProductionManagementSystem.DAL/Repositories/DesignInDeviceRepository.cs
+++ src/ProductionManagementSystem.DAL/Repositories/DesignInDeviceRepository.cs
@@ -1,16 +1,24 @@
-﻿using ProductionManagementSystem.DAL.EF;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductionManagementSystem.DAL.EF;
 using ProductionManagementSystem.Models.Devices;
 
 namespace ProductionManagementSystem.DAL.Repositories
 {
     public interface IDesignInDeviceRepository : IRepository<DesignInDevice>
     {
-        
+        IEnumerable<DesignInDevice> GetDesignsByDeviceId(int deviceId);
     }
     public class DesignInDeviceRepository : Repository<DesignInDevice>, IDesignInDeviceRepository
     {
         public DesignInDeviceRepository(ApplicationContext db) : base(db)
         {
+        }
+
+        public IEnumerable<DesignInDevice> GetDesignsByDeviceId(int deviceId)
+        {
+            return _dbSet.Where(d => d.DeviceId == deviceId);
         }
     }
 }
--- src/ProductionManagementSystem.DAL/Repositories/MontageInDeviceRepository.cs
+++ src/ProductionManagementSystem.DAL/Repositories/MontageInDeviceRepository.cs
@@ -1,17 +1,24 @@
-﻿using ProductionManagementSystem.DAL.EF;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProductionManagementSystem.DAL.EF;
 using ProductionManagementSystem.Models.Devices;
 
 namespace ProductionManagementSystem.DAL.Repositories
 {
     public interface IMontageInDeviceRepository : IRepository<MontageInDevice>
     {
-        
+        public IEnumerable<MontageInDevice> GetMontageByDeviceId(int deviceId);
     }
 
     public class MontageInDeviceRepository : Repository<MontageInDevice>, IMontageInDeviceRepository
     {
         public MontageInDeviceRepository(ApplicationContext db) : base(db)
         {
+        }
+
+        public IEnumerable<MontageInDevice> GetMontageByDeviceId(int deviceId)
+        {
+            return _dbSet.Where(m => m.DeviceId == deviceId);
         }
     }
 }
