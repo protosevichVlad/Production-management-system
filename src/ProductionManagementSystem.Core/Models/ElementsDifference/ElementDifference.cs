1205f73ae0a180fb836c38a18ddb30086432ec78
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-11-26 17:30:23 +0300

    feat: add ElementDifference class

---
+++ src/ProductionManagementSystem.Core/Models/ElementsDifference/ElementDifference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductionManagementSystem.Core.Models.ElementsDifference
+{
+    public class ElementDifference
+    {
+        public ElementType ElementType { get; set; }
+        public int ElementId { get; set; }
+        public int Difference { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
