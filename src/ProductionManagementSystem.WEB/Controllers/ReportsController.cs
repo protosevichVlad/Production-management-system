828dbe50466cfb42be5892ed2fb7599d09c6e630
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-11-29 17:22:21 +0300

    feat: add report controller and change layout pages

---
+++ src/ProductionManagementSystem.WEB/Controllers/ReportsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProductionManagementSystem.Core.Models.Users;
+
+namespace ProductionManagementSystem.WEB.Controllers
+{
+    [Authorize(Roles=RoleEnum.Admin)]
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
