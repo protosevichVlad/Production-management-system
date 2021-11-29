5cadeba1f536fdcab2227f3aade6db122dc718b6
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-11-29 22:18:47 +0300

    rewrite methods with group

--- src/ProductionManagementSystem.Core/Repositories/ElementDifferenceRepository.cs
+++ src/ProductionManagementSystem.Core/Repositories/ElementDifferenceRepository.cs
@@ -12,6 +12,7 @@ namespace ProductionManagementSystem.Core.Repositories
     {
         Task<List<ElementDifference>> GetByPeriodAsync(ElementType type, DateTime from, DateTime to);
         Task<List<ElementDifference>>  GetByPeriodGroupByMonthAsync(ElementType type, DateTime from, DateTime to);
+        Task<List<ElementDifference>>  GetByPeriodGroupByDayAsync(ElementType type, DateTime from, DateTime to);
     }
     
     public class ElementDifferenceRepository : Repository<ElementDifference>, IElementDifferenceRepository
@@ -23,22 +24,42 @@ namespace ProductionManagementSystem.Core.Repositories
         public async  Task<List<ElementDifference>> GetByPeriodAsync(ElementType type, DateTime from, DateTime to)
         {
             return await _dbSet
-                .Where(x => x.ElementType == type && x.DateTime <= to && x.DateTime >= from).ToListAsync();
+                .Where(x => x.ElementType == type && x.DateTime <= to && x.DateTime >= from)
+                .OrderBy(x => x.DateTime)
+                .ToListAsync();
         }
 
         public async Task<List<ElementDifference>> GetByPeriodGroupByMonthAsync(ElementType type, DateTime from,
             DateTime to)
         {
-            return await _dbSet
+            return _dbSet
                 .Where(x => x.ElementType == type && x.DateTime <= to && x.DateTime >= from)
-                .GroupBy(x => new {x.DateTime.Month, x.DateTime.Year})
+                .GroupBy(x => new {x.DateTime.Month, x.DateTime.Year, x.ElementId})
                 .Select(x => new ElementDifference()
                 {
                     DateTime = new DateTime(x.Key.Year, x.Key.Month, 1),
+                    ElementId = x.Key.ElementId,
                     Difference = x.ToList().Sum(x => x.Difference),
                 })
-                .ToListAsync();
+                .ToList()
+                .OrderBy(x => x.DateTime)
+                .ToList();
         }
 
+        public async Task<List<ElementDifference>> GetByPeriodGroupByDayAsync(ElementType type, DateTime from, DateTime to)
+        {
+            return _dbSet
+                .Where(x => x.ElementType == type && x.DateTime <= to && x.DateTime >= from)
+                .GroupBy(x => new {x.DateTime.Month, x.DateTime.Year, x.DateTime.Day, x.ElementId})
+                .Select(x => new ElementDifference()
+                {
+                    DateTime = new DateTime(x.Key.Year, x.Key.Month, x.Key.Day),
+                    ElementId = x.Key.ElementId,
+                    Difference = x.ToList().Sum(x => x.Difference),
+                })
+                .ToList()
+                .OrderBy(x => x.DateTime)
+                .ToList();
+        }
     }
 }
