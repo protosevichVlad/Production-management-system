bf1208f81f1af30d75bd90adacb674adb05963ff
Uladzislau Pratasevich <protosevich2001@gmail.com>
2021-11-29 22:19:06 +0300

    feat: create report service

---
+++ src/ProductionManagementSystem.Core/Services/ReportService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProductionManagementSystem.Core.Models.ElementsDifference;
+using ProductionManagementSystem.Core.Repositories;
+
+namespace ProductionManagementSystem.Core.Services
+{
+    public interface IReportService
+    {
+        Task<List<ElementDifference>> GetMontageMonthReportAsync(int year, int month, int? montageId=null);
+    }
+    
+    public class ReportService : IReportService
+    {
+        private readonly IUnitOfWork _db;
+
+        public ReportService(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<List<ElementDifference>> GetMontageMonthReportAsync(int year, int month, int? montageId=null)
+        {
+            DateTime from = new DateTime(year, month, 1);
+            DateTime to = new DateTime(year, month, 1).AddMonths(1).AddSeconds(-1);
+            return await _db.ElementDifferenceRepository.GetByPeriodGroupByDayAsync(ElementType.Montage, from, to);
+        }
+    }
+}
