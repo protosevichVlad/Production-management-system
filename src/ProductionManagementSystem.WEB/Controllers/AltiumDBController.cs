d3fa005c5597629f173e7027631ed74ab469469f
Uladzislau Pratasevich <protosevich2001@gmail.com>
2022-03-01 19:29:04 +0300

    feat: add carbon ui and vue to project

---
+++ src/ProductionManagementSystem.WEB/Controllers/AltiumDBController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductionManagementSystem.WEB.Controllers
+{
+    public class AltiumDBController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
